11920b8daba2fb7521b9318fbb0f7dd450b3f494
divyendu18 <d4divyendu18@gmail.com>
2020-05-19 23:22:43 +0530

    Updates

---
+++ precache-manifest.0d089bb5a5eb3fedbdc41d4e48b3bc63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af488a3026ad76765095fbcf1cf1fc0",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "f79a8d3aba38a8145e9a",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "95936ca47765bca8bd4c",
+    "url": "/contactmanager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f79a8d3aba38a8145e9a",
+    "url": "/contactmanager/static/js/2.89bccbe7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.89bccbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95936ca47765bca8bd4c",
+    "url": "/contactmanager/static/js/main.2bde5a19.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
